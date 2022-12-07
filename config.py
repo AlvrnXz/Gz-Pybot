e3ed7d31a535392d21eb1403586154d736679211
ᴅᴇᴠɪɴ ᴀʟᴠᴀʀᴇɴ <92626604+alvrnxz@users.noreply.github.com>
2022-12-07 11:07:34 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
 #
-# t.me/SharingUserbot & t.me/ramsupportt
+# t.me/SharingUserbot & t.me/GzSupport
 
 from base64 import b64decode as kodenya
 from distutils.util import strtobool
@@ -16,9 +16,9 @@ from dotenv import load_dotenv
 load_dotenv("config.env")
 
 
-ALIVE_EMOJI = getenv("ALIVE_EMOJI", "👑")
-ALIVE_LOGO = getenv("ALIVE_LOGO", "https://telegra.ph/file/0e030c6e875cfad95cc01.jpg")
-ALIVE_TEKS_CUSTOM = getenv("ALIVE_TEKS_CUSTOM", "Hey bro, I am rams.")
+ALIVE_EMOJI = getenv("ALIVE_EMOJI", "🦋")
+ALIVE_LOGO = getenv("ALIVE_LOGO", "https://graph.org/file/1e53a14e6a70757d1d736.jpg")
+ALIVE_TEKS_CUSTOM = getenv("ALIVE_TEKS_CUSTOM", "Hey bro, I am Gz.")
 API_HASH = getenv("API_HASH")
 API_ID = getenv("API_ID")
 BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
@@ -28,8 +28,8 @@ BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
 BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
 BOT_VER = "0.3.1@dragons"
 BRANCH = "dragons"
-CH_SFS = getenv("CH_SFS", "K0Kb4c0de")
-IG_ALIVE = getenv("IG_ALIVE", "e_rama11")
+CH_SFS = getenv("CH_SFS", "GzSupport")
+IG_ALIVE = getenv("IG_ALIVE", "alvrnxz")
 CHANNEL = getenv("CHANNEL", "userbotch")
 CMD_HANDLER = getenv("CMD_HANDLER", ".")
 DB_URL = getenv("DATABASE_URL", "")
@@ -39,7 +39,7 @@ GIT_TOKEN = getenv(
         "utf-8"
     ),
 )
-GROUP = getenv("GROUP", "ramsupportt")
+GROUP = getenv("GROUP", "GzSupport")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
